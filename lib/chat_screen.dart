e94e9f0834eef4c3ef294ef3e79df348b94a0051
Dao Hoang Son <daohoangson@gmail.com>
2021-06-05 02:23:17 +0700

    Adjust `_buildTextComposer`: padding, hint text, color

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -19,18 +19,28 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   Widget _buildTextComposer() {
-    return Row(
-      children: [
-        Expanded(
-          child: TextField(),
-        ),
-        IconButton(
-          icon: Icon(Icons.send),
-          onPressed: () {
-            print('Send');
-          },
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Send a message',
+              ),
+            ),
+          ),
+          IconTheme(
+            child: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () {
+                print('Send');
+              },
+            ),
+            data: IconThemeData(color: Theme.of(context).accentColor),
+          )
+        ],
+      ),
     );
   }
 }
